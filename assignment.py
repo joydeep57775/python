729d07f219efd06f20567eba6972e22a7b802a7a
joydeep57775 <158074465+joydeep57775@users.noreply.github.com>
2024-02-02 21:53:56 +0530

    homework

---
+++ assignment.py
@@ -0,0 +1,10 @@
+name = input("what is your name: ")
+age = input("what is your age: ")
+weight = input("what is your weight: ")
+gender = input("what is your gender: ")
+
+
+print(name)
+print(age)
+print(weight)
+print(gender)
